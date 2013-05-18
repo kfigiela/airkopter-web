9563222b2dd8a659d9be24da0d11efe7fe99b0f7
Kamil Figiela <kamil.figiela@gmail.com>
2013-05-18 16:00:46 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AirkopterWeb::Application.config.secret_token = 'df079ed5b4a5e15da66ee915a922d8c088aac96c09e21d9861d3e8964a39246c2fa93b14fe477ec276bba8b568c657fcd78988cece4179bf323499d2db9f65d3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AirkopterWeb::Application.config.session_store :cookie_store, key: '_airkopter-web_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AirkopterWeb::Application.config.session_store :active_record_store
